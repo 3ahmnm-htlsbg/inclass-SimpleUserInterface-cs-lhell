ce883e83d29e0d7bf05ae27cf17fbd6ee459c36c
HellLisa <lisahell@nbgum-lhell03-MacBook-Pro.local>
2020-01-07 14:46:52 +0100

    finish Script

--- Assets/MyGame/Scripts/SimpleUserInterface.cs
+++ Assets/MyGame/Scripts/SimpleUserInterface.cs
@@ -1,25 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleUserInterface : MonoBehaviour
 {
-
     public Text result_text;
     public Text result_number;
-    public InputField var_A;
+    public InputField var_a;
     public Button button_a;
     public Button button_b;
     
-    
-    public void ResultText()
+    int intCount = 1;
+
+    public void ConfirmText()
     {
-    
+      result_text.text = var_a.text;
     }
     
     public void CountNumbers()
     {
-    
+      intCount++;
+      result_number.text = intCount.ToString();
     }
-
 }
